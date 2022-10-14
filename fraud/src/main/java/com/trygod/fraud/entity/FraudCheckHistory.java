02f421fb16017918d70bd6bcb41f3c58e1273625
TG <trygod.nwakwasi@decagon.dev>
2022-10-14 16:54:59 +0100

    created the fraud module, entity and fields

---
+++ fraud/src/main/java/com/trygod/fraud/entity/FraudCheckHistory.java
@@ -0,0 +1,33 @@
+package com.trygod.fraud.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class FraudCheckHistory {
+
+    @Id
+    @SequenceGenerator(name = "fraud_id_sequence" , sequenceName = "fraud_id_sequence", allocationSize = 1)
+    @GeneratedValue(generator = "fraud_id_sequence")
+    private Integer id;
+
+    private Integer customerId;
+
+    private Boolean isFraudster;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
